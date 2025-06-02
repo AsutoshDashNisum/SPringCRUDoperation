7871b34118cc3228b92739fa95ffcbd8d713eaa5
AsutoshDashNisum <adash@nisum.com>
2025-06-02 15:12:16 +0530

    First commit

---
+++ src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,43 @@
+package org.example.controller;
+
+
+import org.example.dao.BookDAO;
+import org.example.model.Book;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class HelloController {
+    private BookDAO bookDAO;
+    public void setBookDAO(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+    @RequestMapping("/hello")
+    public ModelAndView sayHello() {
+        String mss = "hello world";
+        return new ModelAndView("hello","message", mss);
+    }
+    @RequestMapping("/books")
+    public ModelAndView listBooks() {
+        List<Book> books = bookDAO.getAllBooks();
+        return new ModelAndView("listBooks", "books", books);
+    }
+    @RequestMapping("/delete")
+    public String deleteBook(@RequestParam("id") int id) {
+        bookDAO.deleteBook(id);
+        return "redirect:/books";  // refresh the list after deletion
+    }
+    @GetMapping("/add")
+    public ModelAndView showAddForm() {
+        return new ModelAndView("addBook", "book", new Book());
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String addBook(@ModelAttribute("book") Book book) {
+        bookDAO.addBook(book);
+        return "redirect:/books";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/addBook.jsp
@@ -0,0 +1,7 @@
+<h2>Add New Book</h2>
+<form action="add" method="post">
+    Title: <input type="text" name="title" required /><br/>
+    Author: <input type="text" name="author" required /><br/>
+    <input type="submit" value="Add Book"/>
+</form>
+<a href="books">Back to Book List</a>
